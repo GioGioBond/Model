5bc1fd2f459aeafd31b0f5c5ee6c8cd472779604
GioGioBond <39087265+GioGioBond@users.noreply.github.com>
2024-04-07 17:45:39 +0800

    加入mask

--- transformer/attention.py
+++ transformer/attention.py
@@ -4,12 +4,16 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+torch.manual_seed(4321)
+np.random.seed(4321)
+
 class Attention(nn.Module):
     def __init__(self, dim_size):
         super(Attention, self).__init__()
         self.wk = torch.nn.Linear(dim_size, dim_size)
         self.wq = torch.nn.Linear(dim_size, dim_size)
         self.wv = torch.nn.Linear(dim_size, dim_size)
+
 
     def attention(self, k, q, v, mask=None):
         '''
@@ -23,38 +27,35 @@ class Attention(nn.Module):
         '''
         print(k.shape, q.shape, v.shape)
 
-        #score = torch.mul(k, q)
-        #score = torch.matmul(k, q)
         score = k @ torch.transpose(q, 0, 1) / math.sqrt(k.shape[-1])
-        if mask:
-            score.masked_fill_(mask, -1e9)
-
+        if mask is not None:
+            score = score.masked_fill_(mask == 0, float('-inf'))
         att_array = nn.functional.softmax(score, dim=1)
 
-        print(att_array.shape)
         att_score = att_array @ v
-        #score = softmax(score)
+
         return score, att_score
 
-    def forward(self, inputs):
+    def forward(self, inputs, mask=None):
         '''
         :param inputs: n * d_s
         :return:
         '''
         k, q, v = self.wk(inputs), self.wq(inputs), self.wv(inputs)
-        print(k.shape)
-        attention_score = self.attention(k, q, v)
+        #print(k.shape)
+
+        attention_score = self.attention(k, q, v, mask)
 
         return attention_score
 
-# k = torch.randint(0,10, (4,10)).float()
-# q = torch.randint(0,10, (4,10)).float()
-# v = torch.randint(0,10, (4,10)).float()
 
-inputs = torch.randint(0, 10, (4, 10)).float()
+inputs = torch.ones((4, 10)).float()
 
 att_function = Attention(inputs.shape[-1])
-att_score = att_function.forward(inputs)
+mask = torch.tril(torch.ones((inputs.shape[0],inputs.shape[0])))
+att_score = att_function.forward(inputs, mask)
 
 print(att_score)
 print(att_score[0].shape, att_score[1].shape)
+
+print(mask)
